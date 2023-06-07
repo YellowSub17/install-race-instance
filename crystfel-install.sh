932119014a0bd7814e702efaf1c74a4d8eb3d055
Your Name <you@example.com>
2023-06-07 08:08:35 +0000

    working gpu install

--- crystfel-install.sh
+++ crystfel-install.sh
@@ -23,13 +23,25 @@ sudo apt install -y build-essential libhdf5-dev libgsl-dev \
                  libeigen3-dev libccp4-dev \
                  ninja-build python3
 
+sudo apt install opencl-headers ocl-icd-opencl-dev -y
+sudo apt isntall nvidia-modprobe
+
+sudo apt-get install linux-headers-$(uname -r)
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
+
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
 
 
 tar -xzf ~/downloads/meson-1.0.2.tar.gz
 cd ~/downloads/crystfel
 git checkout 0.10.2
 
-~/downloads/meson-1.0.2/meson.py build
+~/downloads/meson-1.0.2/meson.py build -Dopencl=enabled
 ninja -C build
 sudo ldconfig
 rm -rf ~/downloads/meson-1.0.2*
